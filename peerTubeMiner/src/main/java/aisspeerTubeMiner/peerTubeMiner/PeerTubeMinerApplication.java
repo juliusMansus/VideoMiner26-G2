330527c93e6c533abbab204d417d6f2213c1f287
hamdifa <hamdifattouh0@gmail.com>
2026-04-26 15:00:54 +0200

    base of the peertube service

--- peerTubeMiner/src/main/java/aisspeerTubeMiner/peerTubeMiner/PeerTubeMinerApplication.java
+++ peerTubeMiner/src/main/java/aisspeerTubeMiner/peerTubeMiner/PeerTubeMinerApplication.java
@@ -2,6 +2,8 @@ package aisspeerTubeMiner.peerTubeMiner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PeerTubeMinerApplication {
@@ -10,4 +12,8 @@ public class PeerTubeMinerApplication {
 		SpringApplication.run(PeerTubeMinerApplication.class, args);
 	}
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
