086da21b2e337bed04dc52d025f050a759c9fe41
Fermin Morera <ferminwebnet@gmail.com>
2026-04-27 16:57:36 +0200

    Implementation of DailyMotion Controller

---
+++ dailyMotionMiner/src/main/java/aiss/dailyMotionMiner/controller/DailyMotionMinerController.java
@@ -0,0 +1,31 @@
+package aiss.dailyMotionMiner.controller;
+
+import aiss.dailyMotionMiner.model.videominer.VMChannel;
+import aiss.dailyMotionMiner.service.DailyMotionMinerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/dailyMotion")
+public class DailyMotionMinerController {
+
+    @Autowired
+    DailyMotionService dailyMotionService;
+
+    @PostMapping("/{id}")
+    public VMChannel fetchAndSendChannel(
+            @PathVariable String channelId,
+            @RequestParam(defaultValue = "10") int maxVideos,
+            @RequestParam(defaultValue = "2") int maxPages) {
+    
+        return dailyMotionService.getChannelAndSendToVideoMiner(channelId, maxVideos, maxPages);
+}
+    @GetMapping("/{id}")
+    public VMChannel getChannel(
+            @PathVariable String id,
+            @RequestParam(defaultValue = "10") int maxVideos,
+            @RequestParam(defaultValue = "2") int maxPages) {
+
+        return dailyMotionService.getChannel(id, maxVideos, maxPages);
+    }
+}
